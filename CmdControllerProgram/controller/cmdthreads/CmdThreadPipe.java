227ab9734806a9386e9d04e41a792c27976447b9
jimmi280586 <jimmiandersen9@gmail.com>
2016-10-11 11:55:24 +0200

    cmd prgram diskpart working2

--- CmdControllerProgram/controller/cmdthreads/CmdThreadPipe.java
+++ CmdControllerProgram/controller/cmdthreads/CmdThreadPipe.java
@@ -1,11 +1,11 @@
-package main;
+package cmdthreads;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.PrintWriter;
 
-class CmdThreadPipe implements Runnable
+public class CmdThreadPipe implements Runnable
 {
 	private final OutputStream outputStream_;
 	private final InputStream inputStream_;
@@ -35,19 +35,6 @@ public void run()
 
 public static void main(String[] args) throws InterruptedException, IOException 
 {
-	 String[] command =
-		    {
-		        "cmd",
-		    };
-		    Process process = Runtime.getRuntime().exec(command);
-		    new Thread(new CmdThreadPipe(process.getErrorStream(), System.err)).start();
-		    new Thread(new CmdThreadPipe(process.getInputStream(), System.out)).start();
-		    PrintWriter print = new PrintWriter(process.getOutputStream());
-		    print.println("shutdown -a");
-		    //for comments displayed in the cmd use single qutations like "shutdown -s -c 'shuttingdown your computer'"
-		    // write any other commands you want here
-		    print.close();
-		    int returnCode = process.waitFor();
-		    System.out.println("Return code = " + returnCode);
+	 
 }
 }
